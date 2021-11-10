7a0e319276148bc9efb35d04fb54217cac5814ca
Jelte <73001216+jojojelte@users.noreply.github.com>
2021-11-10 23:28:48 +0100

    Add files via upload

--- assets/build/app.js
+++ assets/build/app.js
@@ -47,23 +47,5 @@ function scrollFunction() {
   }
 
 
-var modal = document.getElementById("video");
-var vidbtn = document.getElementById("vidbtn");
-var span = document.getElementsByClassName("close")[0];
-
-function showVideo(){
-  modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-  modal.style.display = "none";
-  }
-}
 
 navSlide()
