d17dcf0aded7ffd22ac14e33c1f97c8b2b4cca6c
Jelte <73001216+jojojelte@users.noreply.github.com>
2021-11-10 23:20:23 +0100

    Responsible Changes + JavaScript update

--- assets/build/app.js
+++ assets/build/app.js
@@ -46,4 +46,24 @@ function scrollFunction() {
     hero.style.transitionDuration = "0.5s"
   }
 
+
+var modal = document.getElementById("video");
+var vidbtn = document.getElementById("vidbtn");
+var span = document.getElementsByClassName("close")[0];
+
+function showVideo(){
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+  modal.style.display = "none";
+  }
+}
+
 navSlide()
