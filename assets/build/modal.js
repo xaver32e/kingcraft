490d8df4feeedc09780bc0d0424e17de15b42fa8
Jelte <73001216+jojojelte@users.noreply.github.com>
2021-11-14 21:59:08 +0100

    Add files via upload

--- assets/build/modal.js
+++ assets/build/modal.js
@@ -1,6 +1,7 @@
 var modal = document.getElementById("video");
 var vidbtn = document.getElementById("vidbtn");
 var span = document.getElementsByClassName("close")[0];
+var video = document.getElementById("video")
 
 function showVideo(){
   modal.style.display = "block";
@@ -16,7 +17,7 @@ span.onclick = function() {
 
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.animation = "fade-out 0.5s ease"
+  modal.style.animation = "fade-out 0.5s ease"
   setTimeout(() => {
   modal.style.display = "none";
   }, 400);
